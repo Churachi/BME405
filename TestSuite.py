8e067853b71386bb698d870781edac5d4c86f562
Churachi <cmtam@usc.edu>
2023-11-21 15:40:45 -0800

    File naming convention

--- TestSuite.py
+++ TestSuite.py
@@ -1,6 +1,3 @@
-# Team Members: Chur Tam, Cathy Zhu
-# Github Repo: git@github.com:Churachi/lab07.git
-
 import RPi.GPIO as GPIO
 import time
 import Adafruit_GPIO.SPI as SPI
@@ -21,13 +18,25 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 #Sample Rate Setup
 samplerate = 4000
 array = np.zeros(shape=(1, 1))
+button = False
+fileName = 0
 
 while True:
-    #sound is the data needed, storing sound in array
-    sound = mcp.read_adc(1)
-    print(sound)
-    array[i] = sound
+    if GPIO.input(10) == GPIO.HIGH:
+        time.sleep(500)
+        button = True
+    
+    while button == True:
+        #sound is the data needed, storing sound in array
+        sound = mcp.read_adc(1)
+        print(sound)
+        array.append(sound)
+        time.sleep(0.00025)
 
-    write("example.wav", samplerate, array.astype(np.int16))
-
-    time.sleep(0.00025)
+        if GPIO.input(10) == GPIO.HIGH:
+            time.sleep(500)
+            button = False
+            write("example{fileName}.wav", samplerate, array.astype(np.int16))
+            print("Example %s printed" %(str(fileName)))
+            array = np.zeros(shape=(1, 1))
+            fileName = fileName + 1
