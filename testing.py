761f893c66b8809667f5d6bd87890457fa020548
Churachi <124114408+Churachi@users.noreply.github.com>
2023-11-28 06:37:37 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,10 @@
 import RPi.GPIO as GPIO
 
+# Hardware SPI Configuration
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
 while True:
     # For 5s, read output of sound sensor in 100 ms intervals
     # Turn LED on for 100s if sound sensor is tapped
